5052aa9577b9c906c4995be117aac550eebc9b50
Ryan Kortvelesy <rk627@cam.ac.uk>
2024-07-14 11:59:22 -0700

    added backbone

---
+++ hackathon/policy.py
@@ -0,0 +1,14 @@
+import torch
+
+class Policy:
+
+    def __init__(self, env, world, device):
+        super().__init__()
+        self.env = env
+        self.world = world
+        self.device = device
+
+    def run(self, agent):
+        action = torch.randn(self.world.batch_dim, self.world.dim_p, device=self.device)
+        action = torch.clamp(action, min=-agent.action.u_range, max=agent.action.u_range)
+        return action
---
+++ hackathon/run.py
@@ -0,0 +1,75 @@
+import time
+
+import torch
+
+from vmas import make_env
+from vmas.simulator.utils import save_video
+from policy import Policy
+
+
+def run_heuristic(
+    n_steps: int = 200,
+    n_envs: int = 32,
+    env_kwargs: dict = None,
+    render: bool = False,
+    save_render: bool = False,
+    device: str = "cpu",
+):
+    assert not (save_render and not render), "To save the video you have to render it"
+    if env_kwargs is None:
+        env_kwargs = {}
+    scenario_name = "sampling"
+
+    env = make_env(
+        scenario=scenario_name,
+        num_envs=n_envs,
+        device=device,
+        continuous_actions=True,
+        wrapper=None,
+        # Environment specific variables
+        **env_kwargs,
+    )
+
+    policy = Policy(env=env, world=env.world, device=device)
+
+    frame_list = []  # For creating a gif
+    init_time = time.time()
+    step = 0
+    obs = env.reset()
+    total_reward = 0
+    for _ in range(n_steps):
+        step += 1
+        actions = [None] * len(obs)
+        for i, agent in enumerate(env.agents):
+            actions[i] = policy.run(agent)
+        obs, rews, dones, info = env.step(actions)
+        rewards = torch.stack(rews, dim=1)
+        global_reward = rewards.mean(dim=1)
+        mean_global_reward = global_reward.mean(dim=0)
+        total_reward += mean_global_reward
+        if render:
+            frame_list.append(
+                env.render(
+                    mode="rgb_array",
+                    agent_index_focus=None,
+                    visualize_when_rgb=True,
+                )
+            )
+
+    total_time = time.time() - init_time
+    if render and save_render:
+        save_video(scenario_name, frame_list, 1 / env.scenario.world.dt)
+
+    print(
+        f"It took: {total_time}s for {n_steps} steps of {n_envs} parallel environments on device {device}\n"
+        f"The average total reward was {total_reward}"
+    )
+
+
+if __name__ == "__main__":
+    run_heuristic(
+        n_envs=2,
+        n_steps=200,
+        render=True,
+        save_render=False,
+    )
